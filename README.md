c35984cf2d20e4849ecae13854c3c6dd54464c23
Jess Froholdt Stopa <jess@stopa.dk>
2022-03-07 15:07:54 +0100

    Added support for the new json format

--- README.md
+++ README.md
@@ -28,6 +28,17 @@ public function fields()
 }
 ```
 
+> The image data will be saved as json, so you should make the database field text/json. In your model the field can then be cast as an array.
+
+Given the example above, the `image` property will contain the url and the type of the file as a minimum. It may contain several other properties that are passed from Kontainer.
+
+```php
+$model->image['url']; // https://myaccount.kontainer.com/cdn/2SP4HV/bm-604549.jpg
+$model->image['type']; // 'image'
+$model->image['extension']; // 'jpg'
+...
+```
+
 ## Config
 
 Set the URL to your Kontainer account with `kontainerUrl()`:
@@ -41,3 +52,17 @@ public function fields()
      ];
 }
 ```
+
+## Upgrading from v1
+
+From v2 the data will be saved as json in the database. If you have already used v1 for a while, and you already have data, you should migrate your data to the new json format.
+
+In v1 the data was just the url of the image. As a minimum the new json data should contain the `url` and `type`. A simple database query can do this in one go:
+
+```sql
+UPDATE my_table SET my_field = REPLACE('{"url":"%url%", "type":"image"}', '%url%', my_field)
+```
+
+If you have a mix of images and videos, you might want to add some where clauses to the query, and set the `type` accordingly.
+
+After this you should be able to convert the field type to text or json.
--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,24 +1,16 @@
 <template>
   <default-field :field="field" :errors="errors" :show-help-text="showHelpText">
     <template slot="field">
-      <input
-        :id="field.name"
-        type="hidden"
-        class="w-full form-control form-input form-input-bordered"
-        :class="errorClasses"
-        :placeholder="field.name"
-        v-model="value"
-      />
       <div v-if="value">
         <template v-if="isVideo">
           <video controls width="300">
-            <source :src="value" :type="videoType" />
+            <source :src="value.url" :type="videoType" />
             Sorry, your browser doesn't support embedded videos.
           </video>
         </template>
         <template v-else-if="isImage">
-          <a :href="value" target="_blank"
-            ><img :src="value + '?w=300&h=300'" class="mb-2"
+          <a :href="value.url" target="_blank"
+            ><img :src="value.url + '?w=300&h=300'" class="mb-2"
           /></a>
         </template>
       </div>
@@ -26,6 +18,7 @@
       <button
         @click.prevent="openKontainer"
         class="btn btn-default btn-primary inline-flex items-center relative"
+        v-text="value ? 'Edit' : 'Browse'"
       >
         Browse
       </button>
@@ -48,19 +41,29 @@ export default {
 
   props: ["resourceName", "resourceId", "field"],
 
+  data() {
+    return {
+      token: null,
+    };
+  },
+
   computed: {
     isImage() {
-      return /\.jpg|\.png|\.jpeg|\.gif$/i.test(this.value);
+      return this.value && this.value.type === "image";
     },
     isVideo() {
-      return /\.mp4|\.webm$/i.test(this.value);
+      return this.value && this.value.type === "video";
     },
     videoType() {
-      if (/\.mp4/i.test(this.value)) {
+      if (!this.value) {
+        return;
+      }
+
+      if (/\.mp4/i.test(this.value.url)) {
         return "video/mp4";
       }
 
-      if (/\.webm/i.test(this.value)) {
+      if (/\.webm/i.test(this.value.url)) {
         return "video/webm";
       }
 
@@ -69,6 +72,8 @@ export default {
   },
 
   mounted() {
+    this.token = this.makeid(32);
+
     window.addEventListener("message", this.receive, false);
   },
 
@@ -81,14 +86,14 @@ export default {
      * Set the initial, internal value for the field.
      */
     setInitialValue() {
-      this.value = this.field.value || "";
+      this.value = this.field.value ? JSON.parse(this.field.value) : "";
     },
 
     /**
      * Fill the given FormData object with the field's internal value.
      */
     fill(formData) {
-      formData.append(this.field.attribute, this.value || "");
+      formData.append(this.field.attribute, JSON.stringify(this.value) || "");
     },
 
     openKontainer() {
@@ -97,8 +102,21 @@ export default {
         return;
       }
 
+      // End the Kontainer URL with a trailing slash
+      let url = this.field.kontainerUrl.replace(/\/?$/, "/");
+
+      if (this.value && this.value.folderId) {
+        url += "folder/" + this.value.folderId + "/";
+      }
+
+      if (this.value && this.value.fileId) {
+        url += "file/" + this.value.fileId + "/";
+      }
+
+      url += "?cmsMode=1&cmsToken=" + this.token;
+
       window.open(
-        this.field.kontainerUrl + "?cmsMode=1",
+        url,
         "kontainer",
         "width=1024,height=768,popup,toolbar=no,location=no"
       );
@@ -116,6 +134,10 @@ export default {
         return;
       }
 
+      if (imageData.token !== this.token) {
+          return;
+      }
+
       if (imageData.type !== "image" && imageData.type !== "video") {
         alert("Unknown type");
         return;
@@ -126,11 +148,26 @@ export default {
         return;
       }
 
-      this.value = imageData.url;
+      this.value = imageData;
     },
 
     remove() {
       this.value = null;
+    },
+
+    makeid(length) {
+      let result = "";
+      let characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      let charactersLength = characters.length;
+
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+
+      return result;
     },
   },
 };
